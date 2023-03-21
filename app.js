dccca6819fdd43e92ccb1aca8fec03951a1c4037
deepak25062000 <deepakdongare0102@gmail.com>
2023-03-21 18:07:31 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
+
+// !Import express model
 const express = require('express')
 const app = express();
 
 
+// !Import mongoose model
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
+
+// ! Import CORS model
 const cors = require('cors')
+
+
+//! PORT NUMBER
 let PORT = 4000;
 
 
-// require database model
+//! require database model
 const User = require('./models/user')
 const Post = require('./models/posts')
 
-// middleware
+
+//! middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())  //cross origin resourse sharing
 
+
+// !Database Connection
 const dbURL = "mongodb://localhost:27017/fodiee"
 mongoose.connect(dbURL).then(() => {
     console.log("connected successfullady to database");
 })
 
+
+// ! Signin Sending to Server
 app.post('/signin', async (req, res) => {
     User.findOne({ email: req.body.email }, (err, userData) => {
         if (userData) {
@@ -37,6 +50,8 @@ app.post('/signin', async (req, res) => {
     })
 })
 
+
+//! Signup to Server
 app.post('/signup', async (req, res) => {
     User.findOne({ email: req.body.email }, (err, UserData) => {
         if (UserData) {
@@ -59,6 +74,9 @@ app.post('/signup', async (req, res) => {
     })
 
 })
+
+
+// ! Post Details sending to Server
 app.get('/post', async (req, res) => {
     try {
         const posts = await Post.find()
@@ -68,27 +86,8 @@ app.get('/post', async (req, res) => {
     }
 })
 
-app.get('/user', async(req, res) => {
-    try {
-        const user = await User.find()
-        res.send(user)
-    } catch (error) {
-        console.log(error);
-    }
-})
 
-
-app.get('/post/:id', async (req, res) => {
-    const {id} = req.params
-    try {
-        const singlePost =await Post.findById(id)
-        res.send(singlePost)
-    } catch (error) {
-        res.send(error)
-    }
-})
-
-
+// ! Adding posts in POST/FOOD
 app.post('/add-post', async (req, res) => {
     let postdata = new Post({
         author: req.body.author,
@@ -104,6 +103,63 @@ app.post('/add-post', async (req, res) => {
         res.send({ message: "Failed to add posts" })
     }
 })
+
+
+// !  User Details sending to Server
+app.get('/user', async (req, res) => {
+    try {
+        const user = await User.find()
+        res.send(user)
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+// ! Seding SinglePost Details sending to Server
+app.get('/post/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const singlePost = await Post.findById(id)
+        res.send(singlePost)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+
+// ! Deleting particular  post
+app.delete('/post/:id', async (req, res) => {
+
+    const result = await Post.deleteOne({ _id: req.params.id })
+    res.send(result)
+})
+
+
+// ! Update particular post
+app.put('/post/:id', async (req, res) => {
+    let result = await Post.findByIdAndUpdate(
+        { _id: req.params.id },
+        {
+            $set: { author: req.body.author, title: req.body.title, summary: req.body.summary, image: req.body.image, location: req.body.location, }
+        }
+    )
+    res.send(result)
+})
+
+
+// !to frefill the record in form
+app.get('/post/:id', async (req, res) => {
+    let result = await Post.findOne({ _id: req.params.id });
+    if (result) {
+        res.send(result)
+    } else {
+        res.send({ result: "no record found" })
+    }
+})
+
+
+// ! listining to Server
 app.listen(PORT, () => {
     console.log(`listening to the port ${PORT}`);
 })
